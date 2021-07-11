211e87480baad87cf4b2c26f51d65f8529c6bda4
vn2699 <vn2699@gmail.com>
2021-07-11 20:06:41 +0530

    changes in fedora section

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ done
 if [ "$os" = "fedora" ]
 then
 	echo $passwd | sudo -S dnf update && sudo -S dnf upgrade -y 
-	echo $passwd | sudo -S dnf install vim
+	echo $passwd | sudo -S dnf install vim -y
 	echo $passwd | sudo -S hostnamectl set-hostname --static vn2699
 	echo $passwd | sudo -S dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 	echo $passwd | sudo -S dnf groupupdate core -y
